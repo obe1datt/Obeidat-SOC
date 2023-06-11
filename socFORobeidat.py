6d14a2c82edc99ecf7b140c2217cd65f3b78544d
obe1datt <75526039+obe1datt@users.noreply.github.com>
2023-06-11 21:54:30 +0300

    Add files via upload

--- socFORobeidat.py
+++ socFORobeidat.py
@@ -60,14 +60,14 @@ def search_for_hash_bz(hash_value):
              print("Malware Family :",data['signature'])
              print("Malware Mime Type:",data['file_type_mime'])
              print("Malware Informatiom:",data['file_information'])
-             print("SHA256 Value",data['sha256_hash'])
-             print("SHA2384 Value",data['sha3_384_hash'])
-             print("SHA1 Value",data['sha1_hash'])
-             print("MD5 Value",data['md5_hash'])
-             print("Malware first Seen",data['first_seen'])
-             print("Malware Last Seen",data['last_seen'])
-             print("Delivey Methods",data['delivery_method'])
-             print("Origin County ",data['origin_country'])
+             print("SHA256 Value:",data['sha256_hash'])
+             print("SHA2384 Value:",data['sha3_384_hash'])
+             print("SHA1 Value:",data['sha1_hash'])
+             print("MD5 Value:",data['md5_hash'])
+             print("Malware first Seen:",data['first_seen'])
+             print("Malware Last Seen :",data['last_seen'])
+             print("Delivey Methods :",data['delivery_method'])
+             print("Origin County :",data['origin_country'])
     except:
           print("No Result Found ")
 
@@ -113,7 +113,7 @@ def VT_IP(ip):
 
     for key in alldata :
         if key == 'as_owner':
-        print("ASN owner:" , alldata['as_owner'])
+              print("ASN owner:" , alldata['as_owner'])
         if key == 'asn' :
             print('Autonomous System Numbers: ',alldata['asn'])
         if key == 'country' :
@@ -139,18 +139,136 @@ def VT_IP(ip):
 
 
 ##########################################################################
+
+
+
+
+def MT(hash_val):
+    url = f"https://api.metadefender.com/v4/hash/{hash_val}"
+
+    headers = {
+        'apikey': "b4372a2b95e8f0899cf0e3aefb83e9c2"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    res  = response.json()
+
+    for key in  res:
+        try:
+            if key == 'malware_type' :
+              print('Malware Type :' , res['malware_type'])
+            elif key == 'malware_family' :
+              print('Malware Family :' , res['malware_family'])
+            elif key == 'threat_name' :
+              print('File or Threat Name :' , res['threat_name'])     
+            elif key == 'file_id' :
+              print('File ID :' , res['file_id'])   
+            elif key == 'data_id' :
+              print('Data ID :' , res['data_id'])     
+            elif key == 'file_info':
+                for k in res['file_info']:
+                    print(k , ':' ,res['file_info'][k] ) 
+        except:
+            print("NO Result Found") 
+
+
+###########################################################################
+
+
+
+import requests
+import json
+
+import requests
+import json
+
+def VTF (hash_val):
+
+    VTotal_Key = {'x-apikey':'568991ceebba95d9b80fa01b234762d9a4965d352b8cb3be795fe9a80cda661c'}
+
+    id = hash_val
+    base_url = f"https://www.virustotal.com/api/v3/files/{id}"
+
+
+    req = requests.get(url=base_url , headers=VTotal_Key)
+    res =  req.json()
+    alldata = res.get('data').get('attributes')
+
+    for key in alldata:
+        if key == 'type_description':
+          print('Type of Malware : ' , alldata['type_description'])
+        elif key == 'names' :
+            print("Malware Name or Names")
+            for name in alldata['names']:
+                print('\t',name)
+        elif  key == 'creation_date':
+            print("Malware creation Date :" , alldata['creation_date'])
+        elif key == 'crowdsourced_ids_results':
+            print()
+            print("IOC")
+            print()
+            print()
+            for  ke  in alldata['crowdsourced_ids_results']:
+                if  'alert_context' in ke:
+                    for ioc in ke['alert_context']:
+                        print('\t','Hostname : ',ioc.get('hostname'),' / Protcol :',ioc.get('protocol'),' / Destination Port :',ioc.get('dest_port'), ' / Destination IP :',ioc.get('dest_ip'),' / url:',ioc.get('url'))
+            print()
+        #elif key == 'signature_info':
+        #    print("Signature Information :") 
+        #    for k in alldata['signature_info']:
+        #      print("\t",alldata['signature_info'][k]) 
+        elif key == 'popular_threat_classification':
+            print("Malware Class or  Catagory:")
+            print("\t",alldata['popular_threat_classification']['suggested_threat_label'])   
+
+        elif key == "detectiteasy":
+            print('File information')
+            print('\t' ,  alldata['detectiteasy']['filetype'])
+        elif key == 'size':
+            print('Malware Size : ',alldata['size'])    
+        elif key == 'downloadable':
+            print('Downloadble ' , alldata['downloadable'])
+        elif key == 'first_submission_date':
+            print("First Submission Data" , alldata['first_submission_date'])
+        elif key == 'last_analysis_date':
+            print('Last Analysis Data' , alldata['last_analysis_date'] )
+        elif key == 'last_modification_date':
+            print('Last Modification Date' , alldata['last_modification_date'])
+
+    subdata  =  res.get('links')
+    print("read full report ",subdata)
+
+    subdata1 = res.get('type')
+    print("Type" , subdata1)
+
+
+###########################################################################
 def main():
+  
+    
        
-      
+       
        hash_to_search= input("Enter hash for search : ")
+       hash_to_search = hash_to_search.replace(" ","")
        if hash_to_search != "":
+          print("################ Virus Total Engine ################")
+          VTF(hash_to_search)
+          print("################ Threat Fox Engine  ################")
           search_for_hash(hash_to_search)
-          print("####################################################")
+          print("################    Malware   Bazar ################")
           search_for_hash_bz(hash_to_search)
+          print()
+          print("################### Meta Engine    ####################")
+          print()
+          MT(hash_to_search)
+          #print("################# Virus Total Engines ##############")
+          #vt_engines(hash_to_search)
        else:
            print("Enter A Hash :) ")
            print("Clsoing program ")
            exit
-     
+       
+   
 if __name__ == '__main__':
      main()
